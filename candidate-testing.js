f6712155c4e9cb1db809a1ff0cb7a719841fdf82
jesdone <95374712+jesdone@users.noreply.github.com>
2024-01-10 19:49:04 -0500

    Candidate-testing MULTIPLE QUESTIONS

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,17 +18,33 @@ let candidateAnswer = (" ");
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ",
+                 "True or false: 5 kilometer == 5000 meters? ",
+                 "(5 + 3)/2 * 10 = ? ",
+                 "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+                 "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride",
+                      "true",
+                      "40",
+                      "Trajectory",
+                      "3"]
+let candidateAnswers = (" ");
+
+if (candidateAnswers === correctAnswers){
+  console.log("Your answer: ", candidateAnswers);
+  console.log("Correct answer: ", correctAnswers);
+ }
 
 
-function askForName() {
+function askForName(candidateName) {
   // TODO 1.1b: Ask for candidate's name //
-
+  input.question("Enter your name: ");
+  console.log("Greetings", candidateName);
 }
 
 function askQuestion() {
+  input.question()
+
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 
