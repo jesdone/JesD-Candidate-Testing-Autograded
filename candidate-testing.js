a29d50efee5ab165d91b8a097546943047914313
jesdone <95374712+jesdone@users.noreply.github.com>
2024-01-27 09:12:21 -0500

    Candidate Testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,7 +6,7 @@ const input = require ('readline-sync');
 
 // TODO 1.1b: Ask for candidate's name //
   function askForName() {
-    input.question("Enter your name: ");
+    return input.question("Enter your name: ");
   }
 // TODO 1.1c: Greet candidate using their name //
   console.log("Greetings", candidateName);
@@ -14,14 +14,13 @@ const input = require ('readline-sync');
 
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let askQuestion = input.question("Who was the first American woman in space? ");
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 
 
    // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   function askQuestion() {
-    let candidateAnswer = input.question("Who was the first American woman in space? ");
-  
+    let candidateAnswer = input.question(question);
     if (candidateAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
       console.log("Correct!");
     } else {
@@ -38,17 +37,20 @@ let questions = ["Who was the first American woman in space? ",
                  "True or false: 5 kilometer == 5000 meters? ",
                  "(5 + 3)/2 * 10 = ? ",
                  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-                 "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["sally ride",
+                 "What is the minimum crew size for the ISS? ",
+                ];
+let correctAnswers = ["Sally Ride",
                       "true",
                       "40",
-                      "trajectory",
+                      "Trajectory",
                       "3"]
 
 
-function askQuestions() {
+function askQuestions(questions) {
   let candidateAnswers = [];
-  
+  // if (candidateAnswers.toLowerCase() == correctAnswers[i]){
+  //   grade + 20;
+  // } else (candidateAnswers == "" || candidateAnswers == 0)
   for (let i = 0; i < questions.length; i++) {
     let answer = input.question(questions[i]);
     candidateAnswers.push(answer.toLowerCase());
@@ -58,28 +60,33 @@ function askQuestions() {
 }
 
 //3
-function gradeQuiz(candidateAnswers) {
-  let correctAnswersCount = 0;
+function gradeQuiz(questions, candidateAnswers) {
+  questions=0
+  candidateAnswers=0
+// grade = (questions/candidateAnswers) * 100;
+
+  let correctAnswersCount = 20;
 
   for (let i = 0; i < candidateAnswers.length; i++) {
-    if (candidateAnswers[i] === correctAnswers[i].toLowerCase()) {
-      correctAnswersCount++;
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      correctAnswersCount += 20;
     }
-  }
-
-  let grade = (correctAnswersCount / questions.length) * 100;
+  
+  let grade = (correctAnswersCount / (candidateAnswers.length * 20)) * 100;
   return grade;
 }
-
+}
 
 
 function runProgram() {
   let candidateName = askForName();
-   console.log("Greetings", candidateName); 'if your grade is 80 or above you have passed the Quiz, you grade is, ', grade;
   let candidateAnswers = askQuestions();
-  let grade = gradeQuiz(candidateAnswers);
+  let totalPoints = gradeQuiz(candidateAnswers);
+  let grade = (totalPoints / (candidateAnswers.length * 20)) * 100;
 
-  console.log("your grade is: ", grade);
+  console.log("Greetings", candidateName, 'if your grade is 80 or above you have passed the Quiz, you grade is, ', grade);
+
+  // console.log("your grade is: ", grade);
   if (grade >= 80) {
     console.log("You passed the quiz.");
   } else {
